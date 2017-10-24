b0f9fa0c95603c6ca6a6d76b8e07fe9e85dd8a37
Timo Schlatter <timo.schlatter@nordakademie.de>
2017-10-24 17:22:39 +0200

    Add Service and Controller for Event

---
+++ src/main/java/de/nordakademie/iaa/controller/EventController.java
@@ -0,0 +1,60 @@
+package de.nordakademie.iaa.controller;
+
+
+import de.nordakademie.iaa.model.Event;
+import de.nordakademie.iaa.service.EventService;
+import de.nordakademie.iaa.service.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/events")
+public class EventController {
+
+    private EventService eventService;
+
+    @Autowired
+    public EventController(EventService eventService) {
+        this.eventService = eventService;
+    }
+
+    /**
+     * List all events.
+     *
+     * @return the list of events.
+     */
+    @GetMapping
+    public List<Event> listEvents() {
+        return eventService.listEvents();
+    }
+
+    /**
+     * Saves the given event (either by creating a new one or updating an existing).
+     *
+     * @param event The event to save.
+     */
+    @PutMapping
+    public void saveEvent(@RequestBody Event event) {
+        eventService.saveEvent(event);
+    }
+
+    /**
+     * Deletes the event with given id.
+     *
+     * @param id The id of the event to be deleted.
+     */
+    @DeleteMapping
+    @RequestMapping("/{id}")
+    public ResponseEntity deleteEvent(@PathVariable Long id) {
+        try {
+            eventService.deleteEvent(id);
+            return ResponseEntity.ok(null);
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+}
---
+++ src/main/java/de/nordakademie/iaa/service/EventService.java
@@ -0,0 +1,39 @@
+package de.nordakademie.iaa.service;
+
+import de.nordakademie.iaa.model.Event;
+
+import java.util.List;
+
+
+public interface EventService {
+
+    /**
+     * Stores the given event into the database.
+     *
+     * @param event the event to be saved.
+     */
+    void saveEvent(Event event);
+
+    /**
+     * List all events currently stored in the database.
+     *
+     * @return a list of Event entities. If no event was found an empty list is returned.
+     */
+    List<Event> listEvents();
+
+    /**
+     * Returns the event identified by the given id.
+     *
+     * @param id The identifier.
+     * @return the found entity or {@code null} if no entity was found with given identifier.
+     */
+    Event loadEvent(Long id);
+
+    /**
+     * Deletes the event with the given id.
+     *
+     * @param id The identifier.
+     * @throws EntityNotFoundException if no event could be found for the given id.
+     */
+    void deleteEvent(Long id) throws EntityNotFoundException;
+}
