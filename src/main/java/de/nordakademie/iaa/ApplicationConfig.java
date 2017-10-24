c743b70524b985f06cba65d59b2de125da2e35a7
Timo Schlatter <timo.schlatter@nordakademie.de>
2017-10-24 21:53:47 +0200

    Refactor configuration

--- src/main/java/de/nordakademie/iaa/ApplicationConfig.java
+++ src/main/java/de/nordakademie/iaa/ApplicationConfig.java
@@ -1,12 +1,8 @@
 package de.nordakademie.iaa;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -24,27 +20,14 @@ import static org.springframework.orm.jpa.vendor.Database.H2;
 @ComponentScan
 @Configuration
 @EnableAutoConfiguration
-@EnableConfigurationProperties(DataSourceProperties.class)
-@EntityScan("de.nordakademie.iaa.model")
 @EnableTransactionManagement
 public class ApplicationConfig {
 
     @Bean
+    @ConfigurationProperties("spring.datasource")
     public DataSource getDataSource() {
-        return DataSourceBuilder.create()
-                .username("sa")
-                .password("")
-                .url("jdbc:h2:~/nak;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE")
-                .driverClassName("org.h2.Driver")
-                .build();
+        return DataSourceBuilder.create().build();
     }
-
-//    @Bean
-//    @ConfigurationProperties("spring.datasource")
-//    public DataSource getDataSource() {
-//        DataSource dataSource = DataSourceBuilder.create().build();
-//        return dataSource;
-//    }
 
     @Bean(name = "entityManagerFactory")
     public LocalContainerEntityManagerFactoryBean getEntityManagerFactory() {
@@ -60,7 +43,7 @@ public class ApplicationConfig {
         properties.put("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
         properties.put("hibernate.show_sql", false);
         properties.put("hibernate.format_sql", true);
-        properties.put("hibernate.hbm2ddl.auto", "create");
+        properties.put("hibernate.hbm2ddl.auto", "create-drop");
         entityManagerFactoryBean.setJpaProperties(properties);
         return entityManagerFactoryBean;
     }
