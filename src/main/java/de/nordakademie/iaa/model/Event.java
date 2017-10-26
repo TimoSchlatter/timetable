e23e198572e318df5d316e77311ebe30044869b9
Arvid <arvid.ottenberg@nordakademie.de>
2017-10-26 15:33:40 +0200

    refactor event

--- src/main/java/de/nordakademie/iaa/model/Event.java
+++ src/main/java/de/nordakademie/iaa/model/Event.java
@@ -8,34 +8,48 @@ import javax.persistence.ManyToOne;
 import java.io.Serializable;
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 public class Event extends HasId implements Serializable {
 
-    private HashSet<Docent> docents;
+    // private HashSet<Docent> docents;
     private Group group;
     private LocalDate date;
     private LocalTime startTime;
     private LocalTime endTime;
-    private HashSet<Room> rooms;
     private Subject subject;
+    private Set<Docent> docents;
+    private Set<Room> rooms;
 
     public Event() {}
 
-    public Event(HashSet<Docent> docents, Group group, LocalDate date, LocalTime startTime, LocalTime endTime, HashSet<Room> rooms, Subject subject) {
+    public Event(Set<Room> rooms,Set<Docent> docents, Group group, LocalDate date, LocalTime startTime, LocalTime endTime, Subject subject) {
+        this.rooms = rooms;
         this.docents = docents;
         this.group = group;
         this.date = date;
         this.startTime = startTime;
         this.endTime = endTime;
-        this.rooms = rooms;
         this.subject = subject;
     }
 
-    @NaturalId
-    public HashSet<Docent> getDocents() {
+    @ManyToMany
+    public Set<Room> getRooms() {
+        return rooms;
+    }
+
+    public void setRooms(Set<Room> rooms) {
+        this.rooms = rooms;
+    }
+
+    @ManyToMany
+    public Set<Docent> getDocents() {
         return docents;
+    }
+
+    public void setDocents(Set<Docent> docents) {
+        this.docents = docents;
     }
 
     @NaturalId
@@ -65,11 +79,6 @@ public class Event extends HasId implements Serializable {
         this.endTime = endTime;
     }
 
-    @ManyToMany
-    public void setDocents(HashSet<Docent> docents) {
-        this.docents = docents;
-    }
-
     @NaturalId
     @ManyToOne
     public Group getGroup() {
@@ -78,16 +87,6 @@ public class Event extends HasId implements Serializable {
 
     public void setGroup(Group group) {
         this.group = group;
-    }
-
-    @NaturalId
-    public HashSet<Room> getRooms() {
-        return rooms;
-    }
-
-    @ManyToMany
-    public void setRooms(HashSet<Room> rooms) {
-        this.rooms = rooms;
     }
 
     @ManyToOne
@@ -99,27 +98,4 @@ public class Event extends HasId implements Serializable {
         this.subject = subject;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Event event = (Event) o;
-        if (docents != null ? !docents.equals(event.docents) : event.docents != null) return false;
-        if (group != null ? !group.equals(event.group) : event.group != null) return false;
-        if (date != null ? !date.equals(event.date) : event.date != null) return false;
-        if (startTime != null ? !startTime.equals(event.startTime) : event.startTime != null) return false;
-        if (endTime != null ? !endTime.equals(event.endTime) : event.endTime != null) return false;
-        return rooms != null ? rooms.equals(event.rooms) : event.rooms == null;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = docents != null ? docents.hashCode() : 0;
-        result = 31 * result + (group != null ? group.hashCode() : 0);
-        result = 31 * result + (date != null ? date.hashCode() : 0);
-        result = 31 * result + (startTime != null ? startTime.hashCode() : 0);
-        result = 31 * result + (endTime != null ? endTime.hashCode() : 0);
-        result = 31 * result + (rooms != null ? rooms.hashCode() : 0);
-        return result;
-    }
 }
--- src/test/java/dao/EventDAOTest.java
+++ src/test/java/dao/EventDAOTest.java
@@ -2,14 +2,25 @@ package dao;
 
 import de.nordakademie.iaa.ApplicationConfig;
 import de.nordakademie.iaa.dao.*;
-import de.nordakademie.iaa.model.Event;
+import de.nordakademie.iaa.model.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.HashSet;
+import java.util.List;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by arvid on 25.10.17.
@@ -30,7 +41,7 @@ public class EventDAOTest {
     private EntityManager entityManager;
 
     private Event event;
-/**
+
     @Autowired
     public void setEventDAO(EventDAO eventDAO) {
         this.eventDAO = eventDAO;
@@ -68,10 +79,8 @@ public class EventDAOTest {
         courseDAO.save(course);
         courses.add(course);
 
-        Docent docent = new Docent("test@docent.com", "John", "Doe", "0123123123", "Dr.Dr.", true, 20, courses);
-        docentDAO.save(docent);
-        HashSet<Docent> docents = new HashSet<>();
-        docents.add(docent);
+        Lecture lecture = new Lecture(5,course);
+        lectureDAO.save(lecture);
 
         Group group = new Century("I14a",20,30);
         groupDAO.save(group);
@@ -80,14 +89,17 @@ public class EventDAOTest {
         LocalTime startTime = LocalTime.of(20,0);
         LocalTime endTime = LocalTime.of(22,30);
 
-        HashSet<Room> rooms = new HashSet<>();
-        Room room = new Room(30,"X",45,"999",RoomType.LABORATORY);
+        Docent docent = new Docent("test@docent.com", "John", "Doe", "0123123123", "Dr.Dr.", true, 20, courses);
+        docentDAO.save(docent);
+        HashSet<Docent> docents = new HashSet<>();
+        docents.add(docent);
+
+        Room room = new Room(20,"X", 42, "999", RoomType.COMPUTERROOM);
         roomDAO.save(room);
+        HashSet<Room> rooms = new HashSet<>();
         rooms.add(room);
 
-        Lecture lecture = new Lecture(5,"Kurze Methoden Teil I",course,false);
-        lectureDAO.save(lecture);
-        event = new Event(docents,group,date,startTime,endTime,rooms,lecture);
+        event = new Event(rooms, docents, group, date, startTime, endTime, lecture);
         eventDAO.save(event);
     }
 
@@ -135,6 +147,6 @@ public class EventDAOTest {
         assertEquals(this.event.getSubject(), event.getSubject());
         assertEquals(this.event.getRooms(), event.getRooms());
     }
-**/
+
 }
 
