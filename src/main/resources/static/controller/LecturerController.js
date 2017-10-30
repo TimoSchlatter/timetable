3cce5448f33892a9baa7eede4e0b26edaae2f244
jngh <jonas.gehrke@gmx.de>
2017-10-30 16:34:23 +0100

    LectureController - method Post implemented

--- src/main/resources/static/controller/LecturerController.js
+++ src/main/resources/static/controller/LecturerController.js
@@ -8,15 +8,40 @@ app.controller('LecturerController', function($scope , $http) {
         console.log(response.data)
     });
 
+    $scope.createData = function() {
+        var data = {
+            "forename": $scope.forename,
+            "surname": $scope.surname,
+            "email": $scope.email,
+            "phoneNumber": $scope.phoneNumber,
+            "title": $scope.title,
+            "isPermanentlyEmployed": true
+        };
+        console.log(data);
+        var json = angular.toJson(data);
+        console.log(json);
+
+        $http.post('http://localhost:49999/docents', data)
+            .then(function (data) {
+                $scope.ServerResponse = data;
+            })
+            .then(function (data, status, header, config) {
+                $scope.ServerResponse =  htmlDecode("Data: " + data +
+                    "\n\n\n\nstatus: " + status +
+                    "\n\n\n\nheaders: " + header +
+                    "\n\n\n\nconfig: " + config);
+            });
+    }
+
     $scope.updateData = function (){
-        var data = $.param({
-           forename: $scope.forename,
-            surname: $scope.surname,
-            email: $scope.email,
-            phoneNumber: $scope.phoneNumber,
-            title: $scope.title,
-            isPermanentlyEmployed: true
-        });
+        var data = {
+            "forename": $scope.forename,
+            "surname": $scope.surname,
+            "email": $scope.email,
+            "phoneNumber": $scope.phoneNumber,
+            "title": $scope.title,
+            "isPermanentlyEmployed": true
+        };
 
         $http.put('http://localhost:49999/docents', data)
             .then(function (data) {
