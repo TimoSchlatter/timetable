03673d931c1499aa69b3bc121c36f1e950fbf6ee
Timo Schlatter <timo.schlatter@telekom.de>
2017-10-30 11:48:37 +0100

    Add tests for RoomService

--- src/test/java/service/RoomServiceTest.java
+++ src/test/java/service/RoomServiceTest.java
@@ -5,6 +5,7 @@ import de.nordakademie.iaa.dao.RoomDAO;
 import de.nordakademie.iaa.model.Room;
 import de.nordakademie.iaa.model.RoomType;
 import de.nordakademie.iaa.service.RoomService;
+import de.nordakademie.iaa.service.exception.EntityNotFoundException;
 import de.nordakademie.iaa.service.impl.RoomServiceImpl;
 import org.junit.After;
 import org.junit.Before;
@@ -12,11 +13,16 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.mockito.internal.verification.VerificationModeFactory;
+import org.mockito.verification.VerificationMode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.mockito.Matchers.anyLong;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.internal.verification.VerificationModeFactory.times;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration
@@ -28,20 +34,54 @@ public class RoomServiceTest {
     @Autowired
     private RoomDAO roomDAO;
 
-    @Before
-    public void setup() {
-        Room room = new Room(20,"A",30,"123", RoomType.LECTUREROOM);
-        Mockito.when(roomDAO.findByBuildingAndNumber("A","123")).thenReturn(room);
+    private final String building = "A";
+    private final String number = "123";
+    private final Room room = new Room(20, building, 30, number, RoomType.LECTUREROOM);
+
+    @Test
+    public void testSaveRoom() {
+        roomService.saveRoom(room);
+        Mockito.verify(roomDAO, times(1)).save(room);
     }
 
-    @Test()
-    public void testFindRoomByBuildingAndNumber(){
-        roomService.findByBuildingAndNumber("A","123");
+    @Test
+    public void testLoadRoom() {
+        final Long id = 123L;
+        roomService.loadRoom(id);
+        Mockito.verify(roomDAO, times(1)).findOne(id);
+    }
+
+    @Test
+    public void testListRooms() {
+        roomService.listRooms();
+        Mockito.verify(roomDAO, times(1)).findAll();
+    }
+
+    @Test
+    public void testFindByBuildingAndNumber() {
+        Mockito.when(roomDAO.findByBuildingAndNumber(building, number)).thenReturn(room);
+        roomService.findByBuildingAndNumber(building, number);
+        Mockito.verify(roomDAO, times(1)).findByBuildingAndNumber(building, number);
+    }
+
+    @Test(expected = EntityNotFoundException.class)
+    public void testDeleteNonExistingRoom() throws EntityNotFoundException {
+        final Long id = 123L;
+        Mockito.when(roomDAO.findOne(anyLong())).thenReturn(null);
+        roomService.deleteRoom(id);
+    }
+
+    @Test
+    public void testDeleteExistingRoom() throws EntityNotFoundException {
+        final Long id = 123L;
+        Mockito.when(roomDAO.findOne(anyLong())).thenReturn(room);
+        roomService.deleteRoom(id);
+        Mockito.verify(roomDAO, times(1)).findOne(id);
+        Mockito.verify(roomDAO, times(1)).delete(room);
     }
 
     @After
-    public void verify() {
-        Mockito.verify(roomDAO, VerificationModeFactory.times(1)).findByBuildingAndNumber(Mockito.anyString(),Mockito.anyString());
+    public void clear() {
         Mockito.reset(roomDAO);
     }
 
@@ -58,5 +98,4 @@ public class RoomServiceTest {
             return Mockito.mock(RoomDAO.class);
         }
     }
-
 }
