fc5793ad95d5dd4e73ec07fa69b609acd18f1ab4
jngh <jonas.gehrke@gmx.de>
2017-11-01 19:08:38 +0100

    Kohorte View implementiert

--- src/main/resources/static/controller/LecturerController.js
+++ src/main/resources/static/controller/LecturerController.js
@@ -2,82 +2,39 @@
 
 app.controller('LecturerController', function($scope , $http) {
 
+    // language=JSRegexp
     $scope.ph_numbr = /^(\+?(\d{1}|\d{2}|\d{3})[- ]?)?\d{3}[- ]?\d{3}[- ]?\d{4}$/;
 
-    $http.get('http://localhost:49999/docents').
+    var url = 'http://localhost:49999/docents'
+
+    $http.get(url).
     then(function(response) {
         $scope.docents = response.data;
     });
 
     $scope.addLecturer = function () {
-        this.setCurrentItemId(-1);
-        $scope.forename = '';
-        $scope.surname = '';
-        $scope.email = '';
-        $scope.phone_Number = '';
-        $scope.title = '';
-        $scope.isPermanentlyEmployed = ''
-        $scope.minChangeoverTime = '';
-    }
+        var item = {};
+        this.setCurrentLecturerId(-1);
+        this.setLecturer(item);
+    };
 
     $scope.editLecturer = function (item) {
-        this.setCurrentItemId(item.id)
+        this.setCurrentLecturerId(item.id);
+        this.setLecturer(item);
+    };
+
+    $scope.setLecturer = function (item) {
         $scope.forename = item.forename;
         $scope.surname = item.surname;
         $scope.email = item.email;
-        $scope.phone_Number = item.phoneNumber;
+        $scope.phoneNumber = item.phoneNumber;
         $scope.title = item.title;
         $scope.isPermanentlyEmployed = item.permanentlyEmployed;
         $scope.minChangeoverTime = item.minChangeoverTime;
+    };
 
-    }
-
-    $scope.setCurrentItemId = function (itemId) {
-        $scope.itemId = itemId;
-        $scope.actualModal = ($scope.itemId == -1);
-    }
-
-
-    $scope.deleteData = function() {
-        var url = 'http://localhost:49999/docents/' + $scope.itemId;
-        $http.delete(url)
-            .then(function (data) {
-                $scope.ServerResponse = data;
-            })
-            .then(function (data, status, header, config) {
-                $scope.ServerResponse =  htmlDecode("Data: " + data +
-                    "\n\n\n\nstatus: " + status +
-                    "\n\n\n\nheaders: " + header +
-                    "\n\n\n\nconfig: " + config);
-            });
-        this.setCurrentItemId(-1);
-    }
-
-    $scope.createData = function() {
-        var lecturerData = {
-            "forename": $scope.forename,
-            "surname": $scope.surname,
-            "email": $scope.email,
-            "phoneNumber": $scope.phoneNumber,
-            "title": $scope.title,
-            "permanentlyEmployed": $scope.isPermanentlyEmployed,
-            "minChangeoverTime": $scope.minChangeoverTime
-        };
-
-        $http.post('http://localhost:49999/docents', lecturerData)
-            .then(function (data) {
-                $scope.ServerResponse = data;
-            })
-            .then(function (data, status, header, config) {
-                $scope.ServerResponse =  htmlDecode("Data: " + data +
-                    "\n\n\n\nstatus: " + status +
-                    "\n\n\n\nheaders: " + header +
-                    "\n\n\n\nconfig: " + config);
-            });
-    }
-
-    $scope.updateData = function (){
-        var lecturerData = {
+    $scope.getCurrentLecturerJson = function () {
+        return {
             "forename": $scope.forename,
             "surname": $scope.surname,
             "email": $scope.email,
@@ -86,18 +43,38 @@ app.controller('LecturerController', function($scope , $http) {
             "PermanentlyEmployed": $scope.isPermanentlyEmployed,
             "minChangeoverTime": $scope.minChangeoverTime
         };
+    };
 
-        $http.put('http://localhost:49999/docents', lecturerData)
-            .then(function (data) {
-                $scope.ServerResponse = data;
-            })
-            .then(function (data, status, header, config) {
-                $scope.ServerResponse =  htmlDecode("Data: " + data +
-                    "\n\n\n\nstatus: " + status +
-                    "\n\n\n\nheaders: " + header +
-                    "\n\n\n\nconfig: " + config);
+    $scope.setCurrentLecturerId = function (lecturerId) {
+        $scope.actualModal = (lecturerId === -1);
+    };
+
+    $scope.createData = function() {
+        $http.post(url, $scope.getCurrentLecturerJson())
+            .then(function successCallback(data) {
+                console.log(data);
+            }, function errorCallback(data, status, header, config) {
+                console.log(data, status, header, config);
+            });
+    };
+
+    $scope.updateData = function (){
+        $http.put(url, this.getCurrentLecturerJson())
+            .then(function successCallback(data) {
+                console.log(data);
+            }, function errorCallback(data, status, header, config) {
+                console.log(data, status, header, config);
         });
-        this.setCurrentItemId(-1);
-    }
+        this.setCurrentLecturerId(-1);
+    };
 
-})
+    $scope.deleteData = function() {
+        $http.delete(url + $scope.lecturerId)
+            .then(function successCallback(data) {
+                console.log(data);
+            }, function errorCallback(data, status, header, config) {
+                console.log(data, status, header, config);
+            });
+        this.setCurrentLecturerId(-1);
+    };
+});
