0916c9e2ab41ffc8f3cd9d5ee8a3a8cf1d900d98
Arvid <arvid.ottenberg@nordakademie.de>
2017-11-02 21:56:47 +0100

    add DAO Tests

---
+++ src/test/java/dao/ModuleDAOTest.java
@@ -0,0 +1,90 @@
+package dao;
+
+import de.nordakademie.iaa.Application;
+import de.nordakademie.iaa.dao.ModuleDAO;
+import de.nordakademie.iaa.model.Course;
+import de.nordakademie.iaa.model.Module;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+import static junit.framework.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by arvid on 25.10.17.
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = { Application.class})
+@Transactional
+public class ModuleDAOTest {
+
+
+    private ModuleDAO moduleDAO;
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    private Module module;
+
+    @Autowired
+    public void setModuleDAO(ModuleDAO moduleDAO) {
+        this.moduleDAO = moduleDAO;
+    }
+
+    @Before
+    public void setupData() {
+        module = new Course("R", 234, "Rudern ohne Paddel");
+        moduleDAO.save(module);
+    }
+
+    @After
+    public void tearDown() {
+        entityManager.clear();
+    }
+
+    @Test
+    public void testFindOne() {
+        Module module = moduleDAO.findOne(this.module.getId());
+        compareModules(module);
+    }
+
+    @Test
+    public void testFindAll() {
+        List<Module> modules = moduleDAO.findAll();
+        assertEquals(1, modules.size());
+        for (Module module : modules) {
+            compareModules(module);
+        }
+    }
+
+    @Test
+    public void testDelete() {
+        moduleDAO.delete(this.module);
+        List<Module> modules = moduleDAO.findAll();
+        assertTrue(modules.isEmpty());
+
+    }
+
+    @Test
+    public void testDeleteById() {
+        moduleDAO.deleteById(this.module.getId());
+        List<Module> modules = moduleDAO.findAll();
+        assertTrue(modules.isEmpty());
+    }
+
+    private void compareModules(Module module) {
+        assertEquals(this.module.getShortTitle(), module.getShortTitle());
+        assertEquals(this.module.getTitle(), module.getTitle());
+    }
+}
---
+++ src/test/java/dao/SeminarGroupDAOTest.java
@@ -0,0 +1,89 @@
+package dao;
+
+import de.nordakademie.iaa.Application;
+import de.nordakademie.iaa.dao.SeminarGroupDAO;
+import de.nordakademie.iaa.model.SeminarGroup;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+import static junit.framework.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by arvid on 25.10.17.
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = { Application.class})
+@Transactional
+public class SeminarGroupDAOTest {
+
+
+    private SeminarGroupDAO seminarGroupDAO;
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    private SeminarGroup seminarGroup;
+
+    @Autowired
+    public void setSeminarGroupDAO(SeminarGroupDAO seminarGroupDAO) {
+        this.seminarGroupDAO = seminarGroupDAO;
+    }
+
+    @Before
+    public void setupData() {
+        seminarGroup = new SeminarGroup("Gruppe Lesen", 20, 30);
+        seminarGroupDAO.save(seminarGroup);
+    }
+
+    @After
+    public void tearDown() {
+        entityManager.clear();
+    }
+
+    @Test
+    public void testFindOne() {
+        SeminarGroup seminarGroup = seminarGroupDAO.findOne(this.seminarGroup.getId());
+        compareSeminarGroups(seminarGroup);
+    }
+
+    @Test
+    public void testFindAll() {
+        List<SeminarGroup> seminarGroups = seminarGroupDAO.findAll();
+        assertEquals(1, seminarGroups.size());
+        for (SeminarGroup seminarGroup : seminarGroups) {
+            compareSeminarGroups(seminarGroup);
+        }
+    }
+
+    @Test
+    public void testDelete() {
+        seminarGroupDAO.delete(this.seminarGroup);
+        List<SeminarGroup> seminarGroups = seminarGroupDAO.findAll();
+        assertTrue(seminarGroups.isEmpty());
+
+    }
+
+    @Test
+    public void testDeleteById() {
+        seminarGroupDAO.deleteById(this.seminarGroup.getId());
+        List<SeminarGroup> seminarGroups = seminarGroupDAO.findAll();
+        assertTrue(seminarGroups.isEmpty());
+    }
+
+    private void compareSeminarGroups(SeminarGroup seminarGroup) {
+        assertEquals(this.seminarGroup.getMaximumNumberOfStudents(), seminarGroup.getMaximumNumberOfStudents());
+        assertEquals(this.seminarGroup.getMinChangeoverTime(), seminarGroup.getMinChangeoverTime());
+    }
+}
