b6e34547349680e40260a08620d976fd74730d94
13967 <jonas.jacobsen@nordakademie.de>
2017-11-03 04:23:28 +0100

    ManipleView und Controller überarbeitet.

--- src/main/resources/static/controller/ManipleController.js
+++ src/main/resources/static/controller/ManipleController.js
@@ -5,14 +5,31 @@ app.controller('ManipleController', function ($scope, $http) {
     var cohortsUrl = 'http://localhost:49999/cohorts/';
     var maniplesUrl = 'http://localhost:49999/maniples/';
 
+    /* Sort centuries to get all by default selection */
+    var collectAllManiple = function () {
+        $scope.cohortsAdvanced = angular.copy($scope.cohorts);
+        var allManiples = [];
+        angular.forEach($scope.cohortsAdvanced, function (cohort) {
+            allManiples = allManiples.concat(cohort.maniples);
+        });
+        $scope.cohortsAdvanced.push({name: "Alle", maniples: allManiples});
+    };
+
+    var setModalSelectedValues = function (id) {
+        angular.forEach($scope.cohorts, function (cohort) {
+            angular.forEach(cohort.maniples, function (maniple) {
+                if (id === maniple.id) {
+                    $scope.modalSelectedCohort = cohort;
+                }
+            });
+        });
+    };
+
     var getData = function () {
         $http.get(cohortsUrl).then(function successCallback(response) {
             $scope.cohorts = response.data;
-        }, function errorCallback(response) {
-            console.log(response.statusText);
-        });
-        $http.get(maniplesUrl).then(function successCallback(response) {
-            $scope.maniples = response.data;
+            collectAllManiple();
+            $scope.selectedCohort = $scope.cohortsAdvanced[$scope.cohortsAdvanced.length - 1];
         }, function errorCallback(response) {
             console.log(response.statusText);
         });
@@ -22,14 +39,15 @@ app.controller('ManipleController', function ($scope, $http) {
 
     $scope.setSelectedManiple = function (maniple) {
         $scope.maniple = angular.copy(maniple);
-    };
-
-    $scope.setSelectedCohort = function () {
-        $scope.maniples = angular.copy($scope.cohort);
+        if (maniple.id) {
+            setModalSelectedValues(maniple.id);
+        } else {
+            $scope.modalSelectedCohort = undefined;
+        }
     };
 
     $scope.createData = function () {
-        $http.post(cohortsUrl + this.cohort.id + '/addManiple', JSON.stringify(this.maniple))
+        $http.post(cohortsUrl + this.modalSelectedCohort.id + '/addManiple', JSON.stringify(this.maniple))
             .then(function successCallback(data) {
                 console.log(data);
                 getData();
@@ -49,7 +67,7 @@ app.controller('ManipleController', function ($scope, $http) {
     };
 
     $scope.deleteData = function () {
-        $http.delete(cohortsUrl + this.cohort.id + '/deleteManiple/' +  this.maniple.id)
+        $http.delete(cohortsUrl + this.modalSelectedCohort.id + '/deleteManiple/' + this.maniple.id)
             .then(function successCallback(data) {
                 console.log(data);
                 getData();
