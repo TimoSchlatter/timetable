57ac00f24fa6b1d384c613715acf2b5539b7542e
13967 <jonas.jacobsen@nordakademie.de>
2017-11-03 03:13:35 +0100

    CenturyView überarbeitet. Alle Elemente können nun im Dropdown Menu ausgewählt werden. Default Auswahl hinterlegt.

--- src/main/resources/static/controller/CenturyController.js
+++ src/main/resources/static/controller/CenturyController.js
@@ -6,9 +6,39 @@ app.controller('CenturyController', function ($scope, $http) {
     var maniplesUrl = 'http://localhost:49999/maniples/';
     var centuriesUrl = 'http://localhost:49999/centuries/';
 
+    /* Sort centuries to get all by default selection */
+    var collectAllCenturies = function () {
+        $scope.cohortsAdvanced = angular.copy($scope.cohorts);
+        var allCenturies = [];
+        angular.forEach($scope.cohortsAdvanced, function (cohort) {
+            var cohortCenturies = [];
+            angular.forEach(cohort.maniples, function (maniple) {
+                cohortCenturies = cohortCenturies.concat(maniple.centuries);
+            });
+            cohort.maniples.push({name: "Alle", centuries: cohortCenturies});
+            allCenturies = allCenturies.concat(cohortCenturies);
+        });
+        $scope.cohortsAdvanced.push({name: "Alle", maniples: [{name: "Alle", centuries: allCenturies}]});
+    };
+
+    var setModalSelectedValues = function (id) {
+        angular.forEach($scope.cohorts, function (cohort) {
+            angular.forEach(cohort.maniples, function (maniple) {
+                angular.forEach(maniple.centuries, function (century) {
+                    if (id === century.id) {
+                        $scope.modalSelectedCohort = cohort;
+                        $scope.modalSelectedManiple = maniple;
+                    }
+                });
+            });
+        });
+    };
+
     var getData = function () {
         $http.get(cohortsUrl).then(function successCallback(response) {
             $scope.cohorts = response.data;
+            collectAllCenturies();
+            $scope.setSelectedValues();
         }, function errorCallback(response) {
             console.log(response.statusText);
         });
@@ -16,20 +46,25 @@ app.controller('CenturyController', function ($scope, $http) {
 
     getData();
 
+    $scope.setSelectedValues = function () {
+        if (!$scope.selectedCohort) {
+            $scope.selectedCohort = $scope.cohortsAdvanced[$scope.cohortsAdvanced.length - 1];
+        }
+        $scope.selectedManiple = $scope.selectedCohort.maniples[$scope.selectedCohort.maniples.length - 1];
+    };
+
     $scope.setSelectedCentury = function (century) {
         $scope.century = angular.copy(century);
-    };
-
-    $scope.setSelectedManiple = function () {
-        $scope.centuries = angular.copy($scope.maniple);
-    };
-
-    $scope.setSelectedCohort = function () {
-        $scope.maniples = angular.copy($scope.cohort);
+        if (century.id) {
+            setModalSelectedValues(century.id);
+        } else {
+            $scope.modalSelectedCohort = undefined;
+            $scope.modalSelectedManiple = undefined;
+        }
     };
 
     $scope.createData = function () {
-        $http.post(maniplesUrl + this.maniple.id + '/addCentury', JSON.stringify(this.century))
+        $http.post(maniplesUrl + this.modalSelectedManiple.id + '/addCentury', JSON.stringify(this.century))
             .then(function successCallback(data) {
                 console.log(data);
                 getData();
@@ -49,7 +84,7 @@ app.controller('CenturyController', function ($scope, $http) {
     };
 
     $scope.deleteData = function () {
-        $http.delete(maniplesUrl + this.maniple.id + '/deleteCentury/' + this.century.id)
+        $http.delete(maniplesUrl + this.modalSelectedManiple.id + '/deleteCentury/' + this.century.id)
             .then(function successCallback(data) {
                 console.log(data);
                 getData();
