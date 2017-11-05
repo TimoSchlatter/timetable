533e6968da0d6702f1bb3bc4624fd96e0b1541cd
13967 <jonas.jacobsen@nordakademie.de>
2017-11-05 14:35:28 +0100

    SubjectTypes in die SubjectView integriert

--- src/main/resources/static/controller/SubjectController.js
+++ src/main/resources/static/controller/SubjectController.js
@@ -3,18 +3,23 @@
 app.controller('SubjectController', function ($scope, $http, $filter) {
 
     var subjectsUrl = 'http://localhost:49999/subjects/';
+    var subjectTypesUrl = 'http://localhost:49999/subjecttypes/';
 
-    var setSelectableSubjects = function () {
-        var subjectTypes = $filter('unique')($scope.subjects, 'subjectType');
-        subjectTypes.unshift({subjectType: "Alle"});
-        $scope.selectableSubjects = subjectTypes;
-        $scope.selectedSubjectType = subjectTypes[0];
-    };
+    $http.get(subjectTypesUrl).then(function successCallback(response) {
+        $scope.subjectTypes = response.data;
+        $scope.subjectTypesAdvanced = [];
+        angular.forEach(response.data, function (value, key) {
+            $scope.subjectTypesAdvanced.push({key: key, name: value});
+        });
+        $scope.subjectTypesAdvanced.unshift({key: '', name: "Alle"});
+        $scope.selectedSubjectType = $scope.subjectTypesAdvanced[0];
+    }, function errorCallback(response) {
+        console.log(response.statusText);
+    });
 
     var getData = function () {
         $http.get(subjectsUrl).then(function successCallback(response) {
             $scope.subjects = response.data;
-            setSelectableSubjects();
         }, function errorCallback(response) {
             console.log(response.statusText);
         });
