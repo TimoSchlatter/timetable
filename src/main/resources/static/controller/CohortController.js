0d2ca1a73b78d798a0b5ca0cac773c2a82224507
13967 <jonas.jacobsen@nordakademie.de>
2017-11-08 16:04:55 +0100

    Refactoring der Controller

--- src/main/resources/static/controller/CohortController.js
+++ src/main/resources/static/controller/CohortController.js
@@ -2,14 +2,13 @@
 
 app.controller('CohortController', function ($scope, ConnectionService) {
 
-    $scope.setSelectedCohort = function (cohort) {
-        $scope.cohort = angular.copy(cohort);
-        console.log('Selected Cohort:', $scope.cohort);
-    };
-
     $scope.cohorts = ConnectionService.getCohorts;
     $scope.createCohort = ConnectionService.createCohort;
     $scope.updateCohort = ConnectionService.updateCohort;
     $scope.deleteCohort = ConnectionService.deleteCohort;
 
+    $scope.setSelectedCohort = function (cohort) {
+        $scope.cohort = angular.copy(cohort);
+        console.log('Selected Cohort:', $scope.cohort);
+    };
 });
