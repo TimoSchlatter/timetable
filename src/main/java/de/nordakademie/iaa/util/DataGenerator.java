3bf20037ce8bc4062c8d1341b09a769a27cbc769
Arvid <arvid.ottenberg@nordakademie.de>
2017-11-09 17:15:31 +0100

    extend datagenerator

--- src/main/java/de/nordakademie/iaa/util/DataGenerator.java
+++ src/main/java/de/nordakademie/iaa/util/DataGenerator.java
@@ -256,5 +256,43 @@ public class DataGenerator {
             Event event = new Event(rooms, docents, group, date, startTime, endTime, subject);
             eventService.saveEvent(event);
         }
+
+        date = LocalDate.of(2017, Month.OCTOBER, 31);
+        for (int i = 0; i < 10; i++) {
+            date = date.plusDays(i*7);
+            Set<Room> rooms = new HashSet<>(Arrays.asList(
+                    roomService.findByBuildingAndNumber("A", "001"),
+                    roomService.findByBuildingAndNumber("A", "002")));
+            rooms.forEach(room -> { assert room != null; });
+            Set<Docent> docents = new HashSet<>(Arrays.asList(docentService.findByForenameAndSurname("Soenke", "Stange")));
+            docents.forEach(docent -> { assert docent != null; });
+            Group group = centuryService.findByName("I14a");
+            assert group != null;
+            LocalTime startTime = LocalTime.of(12, 15);
+            LocalTime endTime = LocalTime.of(14, 30);
+            Subject subject = subjectService.findSubjectBySubjectTypeAndModule(SubjectType.EXAM, courseService.findByTitle("Englisch 2"));
+            assert subject != null;
+            Event event = new Event(rooms, docents, group, date, startTime, endTime, subject);
+            eventService.saveEvent(event);
+        }
+
+        for (int i = 0; i < 10; i++) {
+            date = date.plusDays(i*10);
+            Set<Room> rooms = new HashSet<>(Arrays.asList(
+                    roomService.findByBuildingAndNumber("A", "001"),
+                    roomService.findByBuildingAndNumber("A", "002")));
+            rooms.forEach(room -> { assert room != null; });
+            Set<Docent> docents = new HashSet<>(Arrays.asList(docentService.findByForenameAndSurname("Soenke", "Stange")));
+            docents.forEach(docent -> { assert docent != null; });
+            Group group = centuryService.findByName("I14a");
+            assert group != null;
+            LocalTime startTime = LocalTime.of(20, 15);
+            LocalTime endTime = LocalTime.of(22, 30);
+            Subject subject = subjectService.findSubjectBySubjectTypeAndModule(SubjectType.SEMINAR, seminarService.findByTitle("Business-Knigge"));
+            assert subject != null;
+            Event event = new Event(rooms, docents, group, date, startTime, endTime, subject);
+            eventService.saveEvent(event);
+        }
+
     }
 }
