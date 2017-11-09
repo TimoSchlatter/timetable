a7a76a16ee3dae0f87dd601bcb28c97a1dc52157
jngh <jonas.gehrke@gmx.de>
2017-11-09 20:49:28 +0100

    Kollisions Alert implementiert

--- src/main/resources/static/controller/AlertController.js
+++ src/main/resources/static/controller/AlertController.js
@@ -1,8 +1,12 @@
 'use strict';
 
-app.controller('AlertController', function ($scope, AlertService) {
+app.controller('AlertController', function ($scope, AlertService, $rootScope) {
 
     $scope.closeAlert = function () {
        AlertService.clear();
+    }
+
+    $scope.setIgnore = function () {
+        $rootScope.ignoreCollisions = true;
     }
 });
